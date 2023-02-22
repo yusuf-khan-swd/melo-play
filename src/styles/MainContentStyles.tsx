4e3fdba4fe6aa48ec26b76131c962ad9f1a60a58
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-02-22 21:15:04 +0600

    remove commented code

--- src/styles/MainContentStyles.tsx
+++ src/styles/MainContentStyles.tsx
@@ -1,8 +1,6 @@
 import styled from "styled-components";
 
 export const MainBody = styled.div`
-  /* grid-column-start: 3;
-  grid-column-end: 4; */
   background-color: #2e2c2c;
   margin-left: 220px;
   min-height: 100vh;
