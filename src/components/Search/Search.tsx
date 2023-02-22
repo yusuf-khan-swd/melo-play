cb104d276902a057099ac190af7236698efbc11d
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-02-22 22:45:56 +0600

    get the result using spotify api

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const Search = () => {
+  useEffect(() => {
+    const options = {
+      method: "GET",
+      headers: {
+        "X-RapidAPI-Key": "d4872295damsh670d100a2e2ac32p179cc4jsn11255d82ba59",
+        "X-RapidAPI-Host": "spotify81.p.rapidapi.com",
+      },
+    };
+
+    fetch("https://spotify81.p.rapidapi.com/top_200_tracks", options)
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.error(err));
+  }, []);
   return (
     <div>
-      <h2>Search</h2>
+      <h2>
+        Searching Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+        Incidunt minima consequuntur quaerat commodi cumque magni, corrupti
+        asperiores dolorem ipsa at.
+      </h2>
+      <h1>Search 2</h1>
     </div>
   );
 };
