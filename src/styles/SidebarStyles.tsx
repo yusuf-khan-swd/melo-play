f6d77daa521e1f3f06838cb0379e3f0d198a0a4f
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-02-22 11:00:20 +0600

    added style for logo and make flex for side nav items

--- src/styles/SidebarStyles.tsx
+++ src/styles/SidebarStyles.tsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const SidebarContainer = styled.div`
-  grid-column-start: 0;
+  grid-column-start: 1;
   grid-column-end: 3;
 `;
 
@@ -15,14 +15,21 @@ export const SideNav = styled.div`
   background-color: #111;
   overflow-x: hidden;
   padding-top: 20px;
+  text-align: center;
+`;
+
+export const NavLogo = styled.p`
+  font: bold 25px "Poppins";
+  margin: 0 0 8px 0;
 `;
 
 export const SideNavItem = styled.a`
   padding: 6px 8px 6px 16px;
   text-decoration: none;
-  font-size: 25px;
+  font-size: 20px;
   color: #818181;
-  display: block;
+  display: flex;
+  align-items: center;
 
   :hover {
     color: #f1f1f1;
