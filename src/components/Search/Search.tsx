e00f77354b17470134a8eb57f33b4863a348b46b
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-02-23 22:24:05 +0600

    search after search button click

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,7 +1,14 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import { SearchContainer } from "../../styles/SerchStyles";
 
 const Search = () => {
-  useEffect(() => {
+  const [searchResult, setSearchResult] = useState();
+
+  const getFormData = (event) => {
+    event.preventDefault();
+
+    const search = event.target.search.value;
+
     const options = {
       method: "GET",
       headers: {
@@ -9,21 +16,31 @@ const Search = () => {
         "X-RapidAPI-Host": "spotify81.p.rapidapi.com",
       },
     };
-
-    fetch("https://spotify81.p.rapidapi.com/top_200_tracks", options)
+    fetch(
+      `https://spotify81.p.rapidapi.com/search?q=${search}&type=multi&offset=0&limit=10&numberOfTopResults=5`,
+      options
+    )
       .then((response) => response.json())
-      .then((data) => console.log(data))
+      .then((response) => setSearchResult(response))
       .catch((err) => console.error(err));
-  }, []);
+  };
+
+  console.log(searchResult);
+
   return (
-    <div>
+    <SearchContainer>
       <h2>
         Searching Lorem, ipsum dolor sit amet consectetur adipisicing elit.
         Incidunt minima consequuntur quaerat commodi cumque magni, corrupti
         asperiores dolorem ipsa at.
       </h2>
       <h1>Search 2</h1>
-    </div>
+
+      <form onSubmit={getFormData}>
+        <input type="text" name="search" id="" />
+        <button type="submit">Submit</button>
+      </form>
+    </SearchContainer>
   );
 };
 
