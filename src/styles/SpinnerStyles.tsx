4b6dbf1026ffee85a3ac869765063b4a1d113fa6
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-02-23 22:32:41 +0600

    rename spinner

--- src/styles/SpinnerStyles.tsx
+++ src/styles/SpinnerStyles.tsx
@@ -1,6 +1,20 @@
 import styled from "styled-components";
 
-export const Loader = styled.span`
+export const SpinnerScreenCenter = styled.div`
+  height: 50vh;
+  border: 1px solid blue;
+  display: flex;
+  align-items: center;
+
+  /* justify-content: end; */
+  /* justify-content: center; */
+  /* display: flex;
+  justify-content: center;
+  align-items: center;
+  */
+`;
+
+export const Spinner = styled.div`
   width: 48px;
   height: 48px;
   border: 5px solid #fff;
