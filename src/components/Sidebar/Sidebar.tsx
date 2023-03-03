a8ad9ad79389e40246beabdfe97f367b3ddb4e6d
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-03-03 22:01:37 +0600

    convert sidebar repeated item to map function

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { FaHeart, FaHome, FaList, FaSearch } from "react-icons/fa";
-import { NavLink } from "react-router-dom";
+import { json, NavLink } from "react-router-dom";
 import {
   NavItemContent,
   NavLogo,
@@ -9,27 +9,25 @@ import {
 } from "../../styles/SidebarStyles";
 
 const Sidebar = () => {
+  const items = [
+    { path: "/home", name: "Home", icon: <FaHome /> },
+    { path: "/search", name: "Search", icon: <FaSearch /> },
+    { path: "/favorite", name: "Favorites", icon: <FaHeart /> },
+    { path: "/playlist", name: "Playlists", icon: <FaList /> },
+  ];
+
   return (
     <SidebarContainer>
       <SideNav>
         <NavLink to={"/"}>
           <NavLogo>MeloPlay</NavLogo>
         </NavLink>
-        <NavLink to={"/home"}>
-          <FaHome /> <NavItemContent>Home</NavItemContent>
-        </NavLink>
-        <NavLink to={"/search"}>
-          <FaSearch />
-          <NavItemContent>Search</NavItemContent>
-        </NavLink>
-        <NavLink to={"/favorite"}>
-          <FaHeart />
-          <NavItemContent>Favorites</NavItemContent>
-        </NavLink>
-        <NavLink to={"/playlist"}>
-          <FaList />
-          <NavItemContent>Playlists</NavItemContent>
-        </NavLink>
+        {items.map((item, index) => (
+          <NavLink key={index} to={item.path}>
+            {item.icon}
+            <NavItemContent>{item.name}</NavItemContent>
+          </NavLink>
+        ))}
       </SideNav>
     </SidebarContainer>
   );
