2ab451e67e1167394687b4cf173801bb62a2ed49
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-03-09 22:34:15 +0600

    created a local json file for indian music and fetch data from this json file

--- src/components/MainBodySection/MainBodySection.tsx
+++ src/components/MainBodySection/MainBodySection.tsx
@@ -14,19 +14,7 @@ const MainBodySection = () => {
     queryKey: ["tracks"],
     queryFn: async () => {
       try {
-        const options = {
-          method: "GET",
-          headers: {
-            "X-RapidAPI-Key":
-              "9c0cb3cfc5mshf2a02c9550666eep1dbb03jsn296a8bf2c6c2",
-            "X-RapidAPI-Host": "spotify81.p.rapidapi.com",
-          },
-        };
-
-        const res = await fetch(
-          "https://spotify81.p.rapidapi.com/top_200_tracks",
-          options
-        );
+        const res = await fetch("musics.json");
 
         const data = await res.json();
         return data;
