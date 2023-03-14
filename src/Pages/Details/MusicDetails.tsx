14acd042df09558731035206bb7bc56ad277801a
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-03-14 22:23:53 +0600

    show recommended music to details page

--- src/Pages/Details/MusicDetails.tsx
+++ src/Pages/Details/MusicDetails.tsx
@@ -30,11 +30,18 @@ interface Images {
   [index: number]: { url: string };
 }
 
+interface Sources {
+  [index: number]: { url: string };
+}
+
 const MusicDetails = () => {
   const { id } = useParams();
   const { pathname } = useLocation();
   const [isDataLoading, setIsDataLoading] = useState(false);
   const [track, setTrack] = useState<TrackProps>();
+  const [recommendAlbums, setRecommendAlbums] = useState([]);
+  const [trackName, setTrackName] = useState("");
+  // const trackName = track?.name.split(" ")[0];
 
   const options = {
     method: "GET",
@@ -51,6 +58,7 @@ const MusicDetails = () => {
         .then((res) => res.json())
         .then((data) => {
           setTrack(data.tracks[0]);
+          setTrackName(data.tracks[0].name);
           setIsDataLoading(false);
         })
         .catch((error) => {
@@ -59,6 +67,36 @@ const MusicDetails = () => {
         });
     }
   }, []);
+  console.log("trackName", trackName);
+
+  useEffect(() => {
+    if (trackName) {
+      const options = {
+        method: "GET",
+        headers: {
+          "X-RapidAPI-Key":
+            "9c0cb3cfc5mshf2a02c9550666eep1dbb03jsn296a8bf2c6c2",
+          "X-RapidAPI-Host": "spotify81.p.rapidapi.com",
+        },
+      };
+
+      setIsDataLoading(true);
+      fetch(
+        `https://spotify81.p.rapidapi.com/search?q=${trackName}&type=multi&offset=0&limit=50&numberOfTopResults=5`,
+        options
+      )
+        .then((response) => response.json())
+        .then((response) => {
+          console.log("response", response);
+          setRecommendAlbums(response.albums.items);
+          setIsDataLoading(false);
+        })
+        .catch((err) => {
+          console.error(err);
+          setIsDataLoading(false);
+        });
+    }
+  }, [trackName]);
 
   if (isDataLoading) {
     return (
@@ -70,22 +108,24 @@ const MusicDetails = () => {
     );
   }
 
-  let albumUri;
+  // let albumUri;
 
-  if (pathname.includes("album")) {
-    albumUri = id;
-  } else if (pathname.includes("track")) {
-    albumUri = track?.album?.uri.split("spotify:album:")[1];
-  }
+  // if (pathname.includes("album")) {
+  //   albumUri = id;
+  // } else if (pathname.includes("track")) {
+  //   albumUri = track?.album?.uri.split("spotify:album:")[1];
+  // }
 
-  console.log(track);
-  console.log(track?.album?.uri);
+  // console.log("track", track);
+  // console.log(track?.album?.uri);
 
-  console.log({ albumUri: albumUri });
+  // console.log({ albumUri: albumUri });
 
-  console.log(track?.name);
-  console.log(track?.album?.images[0]?.url);
-  console.log(track?.preview_url);
+  // console.log(track?.name);
+  // console.log(track?.album?.images[0]?.url);
+  // console.log(track?.preview_url);
+
+  console.log("recommendAlbums", recommendAlbums);
 
   return (
     <MainBodyContainer>
@@ -104,6 +144,22 @@ const MusicDetails = () => {
             controls
           ></DetailsMusicAudio>
         </DetailsCard>
+        {recommendAlbums?.map(
+          (
+            album: {
+              data: {
+                name: string;
+                coverArt: { sources: Sources };
+              };
+            },
+            index: number
+          ) => (
+            <div key={index}>
+              <img src={album?.data?.coverArt?.sources[1]?.url} alt="" />
+              <p>{album.data.name}</p>
+            </div>
+          )
+        )}
       </DetailsContainer>
     </MainBodyContainer>
   );
